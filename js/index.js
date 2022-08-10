5fde699d715e5f8c2d6456453a788408d8a28ca4
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-08-09 20:32:32 -0500

    Added dice to page

--- js/index.js
+++ js/index.js
@@ -1,34 +1,61 @@
-let character = [
-    {
-        name: "Wizard",
-        img: "images/wizard.png",
-        health: 60,
-        dice: 6
+// let character = [
+//     {
+//         ElementId: document.getElementById('hero'),
+//         name: "Wizard",
+//         img: "images/wizard.png",
+//         health: 60,
+//         dice: 6
+//     },
+
+//     {
+//         elementId: document.getElementById('monster'),
+//         name: "Monster",
+//         img: "images/orc.png",
+//         health: 10,
+//         dice: 64
+//     }
+// ]
+
+const hero =
+{
+    elementId: document.getElementById('hero'),
+    name: "Wizard",
+    img: "images/wizard.png",
+    health: 60,
+    diceRoll: [3,1,4],
+    diceCount: 3
+}
+
+const monster =
+{
+    elementId: document.getElementById('monster'),
+    name: "Monster",
+    img: "images/orc.png",
+    health: 10,
+    diceRoll:[2],
+    diceCount: 1
+}
+
+function renderCharacter(data) {
+    const {elementId, name, img, health, diceRoll, diceCount} = data;
+
+    let diceHtml = "";
+    for(let i = 0; i < diceCount; i++){
+            diceHtml += `<div class="dice">${diceRoll[i]}</div>`
     }
-]
 
-let heroElementId = document.getElementById('hero');
-let heroName = "Wizard"
-let heroImg = "images/wizard.png"
-let heroHealth = "60"
-let heroDice = 6
 
-let monsterElementId = document.getElementById('monster');
-let monsterName = "Orc"
-let monsterImg = "images/orc.png"
-let monsterHealth = "10"
-let monsterDice = 4
-
-function renderCharacter(div, name, img, health, dice) {
-    div.innerHTML = `
+    elementId.innerHTML = `
         <div class="character-card">
             <h4 class="name">${name} </h4>
             <img class="avatar" src="${img}"/>
             <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container"><div class="dice"> ${dice} </div></div>
+            <div class="dice-container">
+                ${diceHtml }
+            </div>
         </div> 
     `
 }
 
-renderCharacter(heroDiv, heroName, heroImg, heroHealth, heroDice)
-renderCharacter(monsterDiv, monsterName, monsterImg, monsterHealth, monsterDice)
+renderCharacter(hero)
+renderCharacter(monster)
