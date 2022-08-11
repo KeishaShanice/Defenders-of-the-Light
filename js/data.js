44828f0f9a1aebe6f0eb7caa0e93a865fc9f498f
KeishaShanice <shanice.lakeisha.sims@gmail.com>
2022-08-11 11:59:31 -0500

    Refactored character data

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const characterData = {
+    hero:
+    {
+        name: "Wizard",
+        img: "images/wizard.png",
+        health: 60,
+        diceCount: 3
+    },
+    
+    monster:
+    {
+        name: "Monster",
+        img: "images/orc.png",
+        health: 10,
+        diceCount: 1
+    }
+}
+
+export {characterData};
+//export default only allow you to export one thing
+//named exports cannot be changed in other files
--- js/index.js
+++ js/index.js
@@ -1,50 +1,13 @@
-function getDiceRollArray(diceCount){
-    return new Array(diceCount).fill(0).map(function(){
-        return Math.floor(Math.random() * 6) + 1
-    })
+import {characterData} from "./data.js";
+import Character from "./Character.js";
+
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
+
+function render(){
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+    
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
 
-function getDiceHtml(diceCount){
-    return getDiceRollArray(diceCount).map(function(num){
-        return `<div class="dice">${num}</div>`
-    }).join("")
-}
-
-const hero =
-{
-    elementId: document.getElementById('hero'),
-    name: "Wizard",
-    img: "images/wizard.png",
-    health: 60,
-    diceCount: 3
-}
-
-const monster =
-{
-    elementId: document.getElementById('monster'),
-    name: "Monster",
-    img: "images/orc.png",
-    health: 10,
-    diceCount: 1
-}
-
-function renderCharacter(data) {
-    const {elementId, name, img, health, diceCount} = data;
-
-    const diceHtml = getDiceHtml(diceCount);
-
-
-    elementId.innerHTML = `
-        <div class="character-card">
-            <h4 class="name">${name} </h4>
-            <img class="avatar" src="${img}"/>
-            <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container">
-                ${diceHtml }
-            </div>
-        </div> 
-    `
-}
-
-renderCharacter(hero)
-renderCharacter(monster)
+render();
